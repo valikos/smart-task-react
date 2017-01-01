9ac14fcf198f2eedb948ee393292a812226c5b87
Valentyn Ostakh <valikos.ost@gmail.com>
2017-01-01 17:44:51 +0200

    Improve auth process

--- src/authentication/formHandlers/loginFormSubmit.js
+++ src/authentication/formHandlers/loginFormSubmit.js
@@ -1,12 +1,17 @@
-import { reset, SubmissionError } from 'redux-form';
-import { loginUser, loginError, rootLocation } from '../actions';
+import { destroy, SubmissionError } from 'redux-form';
+import { setAccessToken } from 'redux-json-api';
+import { loginUser, receiveLogin, loginError, rootLocation } from '../actions';
 import { handleJSONErrorMessage } from '../../lib/handleJSONErrorMessage';
 
 export default function submit(values, dispatch) {
   return dispatch(loginUser(values))
     .then(handleJSONErrorMessage)
-    .then(() => {
-      dispatch(reset('loginForm'));
+    .then(response => {
+      const token = response.headers.get('Authorization');
+      localStorage.setItem('auth_token', token);
+      dispatch(setAccessToken(token));
+      dispatch(receiveLogin(token));
+      dispatch(destroy('loginForm'));
       dispatch(rootLocation());
     }).catch(err => {
       dispatch(loginError(err.message));
