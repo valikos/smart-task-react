389157d62e06ce2139917b52c6aac3a071143eb2
Valentyn Ostakh <valikos.ost@gmail.com>
2017-03-06 16:52:10 +0200

    Remove deprecation messages for redux-sjon-api (#8)

--- src/projects/constants.js
+++ src/projects/constants.js
@@ -1,4 +1,3 @@
-export const NAME = 'projects';
 export const PROJECT_LIST_REQUEST = 'PROJECT_LIST_REQUEST';
 export const PROJECT_LIST_SUCCESS = 'PROJECT_LIST_SUCCESS';
 export const PROJECT_LIST_FAILURE = 'PROJECT_LIST_FAILURE';
